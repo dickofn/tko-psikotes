1b895fc529d5372eb65281e986dbcc43f4b76d66
Dicko Finus <dickofn@gmail.com>
2019-02-14 15:23:47 +0700

    berhitung - no bug for now

---
+++ src/components/Psychotest/DataInputMath.vue
@@ -0,0 +1,37 @@
+<template>
+  <span>
+    <v-layout row wrap>
+      <v-flex offset-md2 xs1 text-xs-right>{{ no }}</v-flex>
+      <v-flex md7 xs11>{{ question }}</v-flex>
+    </v-layout>
+
+    <v-layout row wrap mb-2>
+      <v-flex md5 offset-md3 xs5 offset-xs1>
+        <v-text-field
+          label="Jawaban"
+          name="answer"
+          id="answer"
+          :value="answer"
+          @input="updateAnswer"
+          :rules="[rules.required, rules.number]"
+          required
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['no', 'question', 'answer', 'rules', 'valid'],
+  methods: {
+    updateAnswer (i) {
+      this.$emit('answerUpdated', i)
+      this.$emit('validUpdated', this.valid)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/Psychotest/Mathematic.vue
+++ src/views/Psychotest/Mathematic.vue
@@ -5,7 +5,9 @@
         <v-card>
           <v-card-title primary-title>
             <v-spacer></v-spacer>
-            <h1 class="headline text-uppercase font-weight-light text-xs-center">Psikotes - Berhitung</h1>
+            <h1
+              class="headline text-uppercase font-weight-light text-xs-center"
+            >Psikotes - Berhitung</h1>
             <v-spacer></v-spacer>
           </v-card-title>
           <v-card-text>
@@ -15,7 +17,7 @@
                   <v-flex md8 offset-md2 xs12>
                     <h1>INSTRUKSI</h1>
                     <p>
-                      <b>&rarr;</b> Soal merupakan soal cerita berhitung matematika. Baca soal dengan teliti lalu isi jawaban Anda di bawah soal tersebut. 
+                      <b>&rarr;</b> Soal merupakan soal cerita berhitung matematika. Baca soal dengan teliti lalu isi jawaban Anda di bawah soal tersebut.
                       <br>
                       <b>&rarr;</b> Jawaban merupakan angka, bila jawaban desimal maka jawablah hingga dua angka desimal.
                       <br>
@@ -25,7 +27,186 @@
                 </v-layout>
 
                 <template v-if="isStarted && !isFinished">
-                  <!-- Test inside -->
+                  <data-input-math
+                    :no="'01. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[0]"
+                    @answerUpdated=updateA0
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'02. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[1]"
+                    @answerUpdated=updateA1
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'03. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[2]"
+                    @answerUpdated=updateA2
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'04. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[3]"
+                    @answerUpdated=updateA3
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'05. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[4]"
+                    @answerUpdated=updateA4
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'06. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[5]"
+                    @answerUpdated=updateA5
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'07. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[6]"
+                    @answerUpdated=updateA6
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'08. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[7]"
+                    @answerUpdated=updateA7
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'09. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[8]"
+                    @answerUpdated=updateA8
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'10. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[9]"
+                    @answerUpdated=updateA9
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'11. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[10]"
+                    @answerUpdated=updateA10
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'12. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[11]"
+                    @answerUpdated=updateA11
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'13. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[12]"
+                    @answerUpdated=updateA12
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'14. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[13]"
+                    @answerUpdated=updateA13
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'15. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[14]"
+                    @answerUpdated=updateA14
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'16. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[15]"
+                    @answerUpdated=updateA15
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'17. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[16]"
+                    @answerUpdated=updateA16
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'18. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[17]"
+                    @answerUpdated=updateA17
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'19. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[18]"
+                    @answerUpdated=updateA18
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
+                  <data-input-math
+                    :no="'20. '"
+                    :question="'Seorang anak mempunyai 12 pensil. 6 pensil dipinjam temannya. Berapa pensil sisanya?'"
+                    :answer="a[19]"
+                    @answerUpdated=updateA19
+                    :rules="rules"
+                    :valid="valid"
+                    @validUpdated="updateValid"
+                  ></data-input-math>
                 </template>
 
                 <v-layout row warp justify-center align-center text-xs-center v-if="isFinished">
@@ -53,22 +234,30 @@
       </v-flex>
     </v-layout>
 
-    <v-bottom-nav :value="true" fixed v-if="isStarted && !isFinished" height="20">Waktu yang tersisa {{ prettyTime }}</v-bottom-nav>
+    <v-bottom-nav
+      :value="true"
+      fixed
+      v-if="isStarted && !isFinished"
+      height="20"
+    >Waktu yang tersisa {{ prettyTime }}</v-bottom-nav>
   </v-container>
 </template>
 
 <script>
+import dataInputMath from '../../components/Psychotest/DataInputMath'
+
 export default {
   data () {
     return {
-      time: 480,
+      time: 900,
       timer: null,
       isStarted: false,
       isFinished: false,
-      m: new Array(20),
+      a: new Array(20),
       valid: true,
       rules: {
-        required: v => !!v || '',
+        required: v => !!v || 'Wajib diisi!',
+        number: v => !isNaN(v) || 'Harus berupa angka!'
       }
     }
   },
@@ -100,32 +289,35 @@ export default {
         }, 1000)
       }
     },
-    updateM0 (i) { this.m[0] = i; },
-    updateM1 (i) { this.m[1] = i; },
-    updateM2 (i) { this.m[2] = i; },
-    updateM3 (i) { this.m[3] = i; },
-    updateM4 (i) { this.m[4] = i; },
-    updateM5 (i) { this.m[5] = i; },
-    updateM6 (i) { this.m[6] = i; },
-    updateM7 (i) { this.m[7] = i; },
-    updateM8 (i) { this.m[8] = i; },
-    updateM9 (i) { this.m[9] = i; },
-    updateM10 (i) { this.m[10] = i; },
-    updateM11 (i) { this.m[11] = i; },
-    updateM12 (i) { this.m[12] = i; },
-    updateM13 (i) { this.m[13] = i; },
-    updateM14 (i) { this.m[14] = i; },
-    updateM15 (i) { this.m[15] = i; },
-    updateM16 (i) { this.m[16] = i; },
-    updateM17 (i) { this.m[17] = i; },
-    updateM18 (i) { this.m[18] = i; },
-    updateM19 (i) { this.m[19] = i; },
-    updateM20 (i) { this.m[20] = i; },
+    updateA0 (i) { this.a[0] = i; console.log(this.a[0])},
+    updateA1 (i) { this.a[1] = i; console.log(this.a[1])},
+    updateA2 (i) { this.a[2] = i; console.log(this.a[2])},
+    updateA3 (i) { this.a[3] = i; console.log(this.a[3])},
+    updateA4 (i) { this.a[4] = i; console.log(this.a[4])},
+    updateA5 (i) { this.a[5] = i; console.log(this.a[5])},
+    updateA6 (i) { this.a[6] = i; console.log(this.a[6])},
+    updateA7 (i) { this.a[7] = i; console.log(this.a[7])},
+    updateA8 (i) { this.a[8] = i; console.log(this.a[8])},
+    updateA9 (i) { this.a[9] = i; console.log(this.a[9])},
+    updateA10 (i) { this.a[10] = i; console.log(this.a[10])},
+    updateA11 (i) { this.a[11] = i; console.log(this.a[11])},
+    updateA12 (i) { this.a[12] = i; console.log(this.a[12])},
+    updateA13 (i) { this.a[13] = i; console.log(this.a[13])},
+    updateA14 (i) { this.a[14] = i; console.log(this.a[14])},
+    updateA15 (i) { this.a[15] = i; console.log(this.a[15])},
+    updateA16 (i) { this.a[16] = i; console.log(this.a[16])},
+    updateA17 (i) { this.a[17] = i; console.log(this.a[17])},
+    updateA18 (i) { this.a[18] = i; console.log(this.a[18])},
+    updateA19 (i) { this.a[19] = i; console.log(this.a[19])},
+    updateA20 (i) { this.a[20] = i; console.log(this.a[20])},
     updateValid (i) { this.valid = i; },
     reset () {
       console.log(this.valid)
       this.$refs.form.reset()
     }
+  },
+  components: {
+    dataInputMath
   }
 }
 </script>
