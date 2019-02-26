69f7f14f6ee0f155dd0f375b53ffd5793ec90d6d
Dicko Finus <dickofn@gmail.com>
2019-02-26 16:20:51 +0700

    re-styling

--- src/components/Psychotest/Input/DataInputEnglish.vue
+++ src/components/Psychotest/Input/DataInputEnglish.vue
@@ -2,14 +2,14 @@
   <span>
     <v-layout row wrap align-center>
       <v-flex offset-md1 xs1 text-xs-right>{{ no }}</v-flex>
-      <v-flex md3 xs5>
+      <v-flex md3 xs11>
         <v-radio-group :value="a" @change="updateA" required>
           <v-radio :label="'A. ' + c[0]" value="A"></v-radio>
           <v-radio :label="'B. ' + c[1]" value="B"></v-radio>
         </v-radio-group>
       </v-flex>
       <v-flex xs1 text-xs-right>{{ no2 }}</v-flex>
-      <v-flex md3 xs5>
+      <v-flex md3 xs11>
         <v-radio-group :value="a2" @change="updateA2" required>
           <v-radio :label="'A. ' + c2[0]" value="A"></v-radio>
           <v-radio :label="'B. ' + c2[1]" value="B"></v-radio>
