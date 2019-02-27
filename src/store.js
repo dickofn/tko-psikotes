f53919b74ae3dc665408abda1e9466b44014fd3e
Dicko Finus <dickofn@gmail.com>
2019-02-27 14:24:52 +0700

    setting up modular store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+import user from './store/user'
+import test from './store/test'
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    user,
+    test
   }
-})
+});
---
+++ src/store/test.js
@@ -0,0 +1,7 @@
+export default {
+  state: {
+    note: "emptiness"
+  },
+  mutations: {},
+  actions: {}
+};
