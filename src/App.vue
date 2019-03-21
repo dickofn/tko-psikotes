bfb37c47c0cf3b74ba6e8017392e2c99e1809ed7
Dicko Finus <dickofn@gmail.com>
2019-03-21 10:41:39 +0700

    header logo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-spacer></v-spacer>
+      <v-img :src="'/assets/logo-big.png'" max-height="70%" contain v-if="!(screenSize=='xs' || screenSize=='sm')"></v-img>
+      <v-spacer v-if="!(screenSize=='xs' || screenSize=='sm')"></v-spacer>
       <v-toolbar-title class="headline text-uppercase">
         <span>Psikotes&nbsp;</span>
         <span class="font-weight-light">Tokoonderdil</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-img :src="'/assets/logo-small.png'" height="100%" :contain="(screenSize=='xs' || screenSize=='sm')"></v-img>
     </v-toolbar>
 
     <v-content>
@@ -18,5 +20,10 @@
 <script>
 export default {
   name: 'App',
+  computed: {
+    screenSize () {
+      return this.$vuetify.breakpoint.name
+    }
+  }
 }
 </script>
